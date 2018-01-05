d1c18f25d026a6dcaad180f192746573d2ceec42
Andela Developer <andeladeveloper@Andelas-MacBook-Pro-72.local>
2018-01-05 09:07:36 +0100

    adding configurations

--- src/generate-qr-code-image.js
+++ src/generate-qr-code-image.js
@@ -1,37 +1,31 @@
-import Syncano from "syncano-server";
-import qr from "qr-image";
+import Syncano from 'syncano-server';
+import qr from 'qr-image';
 
-export default async ctx => {
+export default async (ctx) => {
   const { response } = Syncano(ctx);
 
-  const checkQRImage = () => {
-    return new Promise((resolve, reject) => {
-      if (!ctx.args.text) {
-        return reject(
-          response.json({
-            message: "Enter text to generate QR Code."
-          })
-        );
-      } else {
-        const qrImage = qr.image(ctx.args.text, ctx.args.options || {});
-        return resolve(qrImage);
-      }
-    });
-  };
+  const checkQRImage = () => new Promise((resolve, reject) => {
+    if (!ctx.args.text) {
+      return reject(response.json({
+        message: 'Enter text to generate QR Code.'
+      }));
+    }
+    const qrImage = qr.image(ctx.args.text, ctx.args.options || {});
+    return resolve(qrImage);
+  });
 
   try {
-    if (ctx.meta.request.REQUEST_METHOD === "POST") {
+    if (ctx.meta.request.REQUEST_METHOD === 'POST') {
       const getQRImage = await checkQRImage();
       return response.json({
-        message: "QR Image Generated.",
+        message: 'QR Image Generated.',
         statusCode: 200,
         data: getQRImage
       });
     }
-    throw new Error("Make ource.", 400);
   } catch (error) {
     return response.json({
-      message: "Make sure to use `POST` request method for generating QRCode",
+      message: 'Make sure to use `POST` request method for generating QRCode',
       statusCode: 400,
       data: error
     });
--- test/generateQRCodeTest.js
+++ test/generateQRCodeTest.js
@@ -1,50 +1,49 @@
-import { assert, expect } from "chai";
-import { run, generateMeta } from "syncano-test";
+import { assert, expect } from 'chai';
+import { run, generateMeta } from 'syncano-test';
 
-let meta = generateMeta("generate-qr-code-image");
+const meta = generateMeta('generate-qr-code-image');
 let args = {};
 
-describe("Generate QR Code", () => {
-  it("returns an error if a `GET` request method is passed to Url", done => {
-    meta.request.REQUEST_METHOD = "GET";
-    run("generate-qr-code-image", {
+describe('Generate QR Code', () => {
+  it('returns an error if a `GET` request method is passed to Url', (done) => {
+    meta.request.REQUEST_METHOD = 'GET';
+    run('generate-qr-code-image', {
       meta
-    }).then(response => {
-      expect(response.data.message).to.equal(
-        "Make sure to use `POST` request method for generating QRCode"
-      );
+    }).then((response) => {
+      expect(response.data.message).to.equal('Make sure to use `POST`' +
+      ' request method for generating QRCode');
       expect(response.data.statusCode).to.equal(400);
       expect(response.data.data).to.be.empty;
       done();
     });
   });
 
-  it("returns error if text argument is not found", done => {
-    meta.request.REQUEST_METHOD = "POST";
+  it('returns error if text argument is not found', (done) => {
+    meta.request.REQUEST_METHOD = 'POST';
     args = {
-      options: { type: "png" }
+      options: { type: 'png' }
     };
-    run("generate-qr-code-image", {
+    run('generate-qr-code-image', {
       args,
       meta
-    }).then(response => {
-      expect(response.data.message).to.equal("Enter text to generate QR Code.");
+    }).then((response) => {
+      expect(response.data.message).to.equal('Enter text to generate QR Code.');
       done();
     });
   });
 
-  it("returns error if text argument is not found", done => {
-    meta.request.REQUEST_METHOD = "POST";
+  it('returns error if text argument is not found', (done) => {
+    meta.request.REQUEST_METHOD = 'POST';
     args = {
-      text: "Testing QR Code Generator.",
-      options: { type: "png" }
+      text: 'Testing QR Code Generator.',
+      options: { type: 'png' }
     };
-    run("generate-qr-code-image", {
+    run('generate-qr-code-image', {
       args,
       meta
-    }).then(response => {
-      expect(response.data.message).to.equal("QR Image Generated.");
-      expect(response.data).to.have.property("data").to.not.be.empty;
+    }).then((response) => {
+      expect(response.data.message).to.equal('QR Image Generated.');
+      expect(response.data).to.have.property('data').to.not.be.empty;
       done();
     });
   });
